35590b0d5f3c860dc956cac404456f51227eeaba
Leon Lee <leon.lee@berkeley.edu>
2016-12-17 11:09:49 -0800

    update gulp & npm configs

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var config = require('./gulp.config');
 var gulp = require('gulp');
+var del = require('del');
 var shell = require('gulp-shell');
 
 
 gulp.task('build-html', shell.task('sphinx-build -b html -d build/doctrees source doc'));
 
-gulp.task('build-epub', shell.task('sphinx-build -b epub -d build/doctrees source build/epub'))
-gulp.task('default', ['build-html'], function() {
-  gulp.watch(['./source/**/*.rst', './source/**/*.md', './source/conf.py'], ['build-html']);
+gulp.task('clean', function () {
+    return del(['/doc/*']);
+});
+
+gulp.task('build-epub', shell.task('sphinx-build -b epub -d build/doctrees source build/epub'));
+
+gulp.task('default', ['clean', 'build-html'], function() {
+  gulp.watch(['./source/**/*.rst', './source/**/*.md', './source/conf.py'], ['clean', 'build-html']);
 });
--- source/profession/languages/typescript.rst
+++ source/profession/languages/typescript.rst
@@ -5,3 +5,8 @@ From Official `Documentation<https://www.typescriptlang.org]>`_
 
 1. Quick Start
 ===============
+TypeScript features
+
+- Type Annotation: Record the intended contract of the function or variable.
+- Interfaces: Define complex data type.
+- Classes: Support class-based object-oriented programming.
